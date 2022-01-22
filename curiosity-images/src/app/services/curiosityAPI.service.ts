4a83d4fecff757fade286ae7614dacf713feb57b
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-22 23:39:11 +0000

    Update api call

--- curiosity-images/src/app/services/curiosityAPI.service.ts
+++ curiosity-images/src/app/services/curiosityAPI.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { CuriosityDataModel } from 'src/shared/models/curiosityData.model';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { CuriosityDataModel, PhotosModel } from 'src/shared/models/curiosityData.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuriosityAPIService {
-  public curiosityData$ = new BehaviorSubject<Array<CuriosityDataModel>>(undefined);
-  public selected$ = new BehaviorSubject<CuriosityDataModel>(undefined);
+  
+  public curiosityData$ = new BehaviorSubject<Array<CuriosityDataModel>>([]);
+  public selected$ = new BehaviorSubject<Array<CuriosityDataModel>>([]);
   private readonly API_BASE_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?';
   private readonly API_KEY_PARAMETER = "&api_key=SJtLy0wNppl58fwcHcMcFKo9i4tkASgJlgLSmXkJ"
 
@@ -17,19 +18,27 @@ export class CuriosityAPIService {
     private http: HttpClient
   ) { }
 
-  public getCuriosityAPIData(): void {
-    const url = `${this.API_BASE_URL}?earth_date=2015-6-3${this.API_KEY_PARAMETER}`;
-    this.http.get<Array<CuriosityDataModel>>(url).pipe(
-      tap(data => this.curiosityData$.next(data))
-    ).subscribe();
+  // public getCuriosityAPIData(): Observable<CuriosityDataModel[]> {
+  //   const url = `${this.API_BASE_URL}earth_date=2015-6-3${this.API_KEY_PARAMETER}`;
+  //   return this.http.get<Array<any>>(url).pipe(
+  //     tap(data => console.log(data)),
+  //     tap(data => this.curiosityData$.next(data))
+  //   ).subscribe();
+  // }
+
+  public getData(): Observable<CuriosityDataModel[]> {
+    const url = `${this.API_BASE_URL}earth_date=2015-6-3${this.API_KEY_PARAMETER}`;
+    return this.http.get<Array<PhotosModel>>(url)
+    .pipe(
+      map((data: any) => data.photos)
+    );
   }
 
-  public getSelecterdCuriosityAPIData(id: number | string): void {
-    const url = `${this.API_BASE_URL}/earth_date=2015-6-3${this.API_KEY_PARAMETER}`;
-    this.http.get<CuriosityDataModel>(url).pipe(
-      tap(recipe => this.selected$.next(recipe))
-    ).subscribe();
-  }
+  // public getSelecterdCuriosityAPIData(id: number | string): void {
+  //   const url = `${this.API_BASE_URL}/earth_date=2015-6-3${this.API_KEY_PARAMETER}`;
+  //   this.http.get<CuriosityDataModel>(url).pipe(
+  //     tap(recipe => this.selected$.next(recipe))
+  //   ).subscribe();
+  // }
 }
-https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY
 
--- curiosity-images/src/shared/models/curiosityData.model.ts
+++ curiosity-images/src/shared/models/curiosityData.model.ts
@@ -2,10 +2,14 @@ import { CameraModel } from "./camera.model";
 import { RoverModel } from "./rover.model";
 
 export interface CuriosityDataModel {
-    id: number;
-    sol: number;
-    camera: CameraModel[];
-    img_src: string;
-    earth_date: string;
-    rover: RoverModel[];
+    id?: number;
+    sol?: number;
+    camera?: CameraModel[];
+    img_src?: string;
+    earth_date?: string;
+    rover: RoverModel;
+};
+
+export interface PhotosModel {
+    photos: CuriosityDataModel;
 };
