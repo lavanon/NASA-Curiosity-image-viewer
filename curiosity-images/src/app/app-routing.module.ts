b2c935e0b9999803633100aa6560821ad18d710d
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-22 17:56:03 +0000

    simple landing page

--- curiosity-images/src/app/app-routing.module.ts
+++ curiosity-images/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPage } from './pages/landing/landing.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'landing-page',
+    component: LandingPage,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'landing-page'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- curiosity-images/src/app/app.module.ts
+++ curiosity-images/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPage } from './pages/landing/landing.page';
 
 @NgModule({
   declarations: [
+    LandingPage,
     AppComponent
   ],
   imports: [
