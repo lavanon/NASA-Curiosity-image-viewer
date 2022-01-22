b6b11854df038931056c9eea346e81f79ca50b4f
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-22 23:40:09 +0000

    Add some Angular material imports

--- curiosity-images/src/app/app.module.ts
+++ curiosity-images/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPage } from './pages/landing/landing.page';
-
+import { HomePage } from './pages/home/home.page';
+import { HttpClientModule } from '@angular/common/http';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
+    AppComponent,
     LandingPage,
-    AppComponent
+    HomePage,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule, MatInputModule, MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
