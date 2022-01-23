cee9e9b3343c378ddc47f81bead69e1b57e8ce1c
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-23 16:37:58 +0000

    modules to build out components

--- curiosity-images/src/app/app.module.ts
+++ curiosity-images/src/app/app.module.ts
@@ -10,6 +10,9 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +25,13 @@ import {MatInputModule} from '@angular/material/input';
     BrowserAnimationsModule,
     HttpClientModule,
     MatDatepickerModule,
-    MatNativeDateModule, MatInputModule, MatFormFieldModule,
+    MatNativeDateModule, 
+    MatInputModule, 
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- curiosity-images/src/app/pages/home/home.page.ts
+++ curiosity-images/src/app/pages/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map, tap } from 'rxjs';
 import { Observable } from 'rxjs';
 import { CuriosityAPIService } from 'src/app/services/curiosityAPI.service';
-import { CuriosityDataModel } from 'src/shared/models/curiosityData.model';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +10,42 @@ import { CuriosityDataModel } from 'src/shared/models/curiosityData.model';
   styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
-
+  public form!: FormGroup;
+  selected!: string;
+  doWeHaveImages: boolean = false;
+  date = new FormControl(new Date());
   constructor(
-    private curiosityAPIService: CuriosityAPIService
+    private curiosityAPIService: CuriosityAPIService, 
   ) { }
 
   public curiosityData$!: Observable<any[]>;
 
+onSavePost() {
+  console.log(this.selected)
+  console.log(this.date.value);
+
+  const newDate = this.date.value.toString().split(" ")
+  const year = newDate[3];
+  const month = new Date(Date.parse(newDate[1] +" 1, 2012")).getMonth()+1;
+  const day = newDate[2];
+  const fullDate = `${year}-${month}-${day}`
+  console.log(fullDate);
+  this.curiosityData$ = this.curiosityAPIService.getData(fullDate, this.selected).pipe(
+    tap(data => console.log(data.length)),
+    tap(data => this.doWeHaveImages = data.length ? true : false)
+
+  );
+    
+}
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: [Validators.required] }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+    });
+
     this.curiosityData$ = this.curiosityAPIService.getData().pipe(
-      tap(data => console.log(data))
+      tap(data => console.log(data.length)),
+      tap(data => this.doWeHaveImages = data.length ? true : false)
     );
   }
 }
