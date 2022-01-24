b4a233e185a3ea6c11552e9080206b34e4ca24eb
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-24 13:52:44 +0000

    white space

--- curiosity-images/src/app/components/rover-list-item/rover-list-item.component.spec.ts
+++ curiosity-images/src/app/components/rover-list-item/rover-list-item.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RoverListItemComponent } from './rover-list-item.component';
 
 describe('RoverListItemComponent', () => {
-  let component: RoverListItemComponent;
-  let fixture: ComponentFixture<RoverListItemComponent>;
+    let component: RoverListItemComponent;
+    let fixture: ComponentFixture<RoverListItemComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RoverListItemComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ RoverListItemComponent ]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RoverListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RoverListItemComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 });
--- curiosity-images/src/app/components/rover-list/rover-list.component.ts
+++ curiosity-images/src/app/components/rover-list/rover-list.component.ts
@@ -2,16 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CuriosityDataModel } from 'src/shared/models/curiosityData.model';
 
 @Component({
-  selector: 'app-rover-list',
-  templateUrl: './rover-list.component.html',
-  styleUrls: ['./rover-list.component.scss']
+    selector: 'app-rover-list',
+    templateUrl: './rover-list.component.html',
+    styleUrls: ['./rover-list.component.scss']
 })
 export class RoverListComponent implements OnInit {
-  @Input() public rovers: Array<CuriosityDataModel> | undefined;
-  @Input() public doWeHaveImages: boolean | undefined;
-  
-  constructor() { }
+    @Input() public rovers: Array<CuriosityDataModel> | undefined;
+    @Input() public doWeHaveImages: boolean | undefined;
+    
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 }
