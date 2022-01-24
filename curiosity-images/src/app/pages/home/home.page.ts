df020b7bce6f4af4e33654d00ecaafa8b8b64cb5
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-24 12:28:16 +0000

    Clean up function and html

--- curiosity-images/src/app/pages/home/home.page.ts
+++ curiosity-images/src/app/pages/home/home.page.ts
@@ -1,51 +1,44 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
 import { Observable } from 'rxjs';
 import { CuriosityAPIService } from 'src/app/services/curiosityAPI.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
-  public form!: FormGroup;
-  selected!: string;
-  doWeHaveImages: boolean = false;
-  date = new FormControl(new Date());
-  constructor(
-    private curiosityAPIService: CuriosityAPIService, 
-  ) { }
+    public cameraSelected!: string;
+    public date = new FormControl(new Date());
 
-  public curiosityData$!: Observable<any[]>;
+    public doWeHaveImages: boolean = false;
 
-onSavePost() {
-  console.log(this.selected)
-  console.log(this.date.value);
+    public curiosityData$!: Observable<any[]>;
 
-  const newDate = this.date.value.toString().split(" ")
-  const year = newDate[3];
-  const month = new Date(Date.parse(newDate[1] +" 1, 2012")).getMonth()+1;
-  const day = newDate[2];
-  const fullDate = `${year}-${month}-${day}`
-  console.log(fullDate);
-  this.curiosityData$ = this.curiosityAPIService.getData(fullDate, this.selected).pipe(
-    tap(data => console.log(data.length)),
-    tap(data => this.doWeHaveImages = data.length ? true : false)
+    constructor(
+        private curiosityAPIService: CuriosityAPIService, 
+    ) { }
 
-  );
-    
-}
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      title: new FormControl(null, { validators: [Validators.required] }),
-      content: new FormControl(null, { validators: [Validators.required] }),
-    });
+    ngOnInit(): void {
+        this.curiosityData$ = this.curiosityAPIService.getData().pipe(
+            tap(data => this.doWeHaveImages = data.length ? true : false)
+        );
+    }
 
-    this.curiosityData$ = this.curiosityAPIService.getData().pipe(
-      tap(data => console.log(data.length)),
-      tap(data => this.doWeHaveImages = data.length ? true : false)
-    );
-  }
+    public searchImages() {
+        this.curiosityData$ = this.curiosityAPIService.getData(this._trasformDate(), this.cameraSelected).pipe(
+            tap(data => this.doWeHaveImages = data.length ? true : false)
+        );
+    }
+
+    private _trasformDate() {
+        // this.date looks like => Sat Jul 03 2021 00:00:00 GMT+0100 (Western European Summer Time)
+        const newDate = this.date.value.toString().split(" ")
+        const year = newDate[3];
+        const month = new Date(Date.parse(newDate[1] +" 1, 2012")).getMonth()+1;
+        const day = newDate[2];
+        return`${year}-${month}-${day}`
+    }
 }
