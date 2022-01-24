269393ae31cb131147dd678d77493d22ee0ae609
lavanon <35270586+lavanon@users.noreply.github.com>
2022-01-24 13:39:26 +0000

    Clean up and add project images

--- curiosity-images/src/app/components/rover-list-item/rover-list-item.component.ts
+++ curiosity-images/src/app/components/rover-list-item/rover-list-item.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rover-list-item',
-  templateUrl: './rover-list-item.component.html',
-  styleUrls: ['./rover-list-item.component.scss']
+    selector: 'app-rover-list-item',
+    templateUrl: './rover-list-item.component.html',
+    styleUrls: ['./rover-list-item.component.scss']
 })
 export class RoverListItemComponent implements OnInit {
-  @Input() public rover: any | undefined;
+    @Input() public rover: any | undefined;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 }
